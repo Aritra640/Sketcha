d8471bcaf2cd95cd970071c81ea4005657f25d0d
Aritra640 <chatterjeearitra15@gmail.com>
2026-02-13 03:09:42 +0530

    minor changes

--- apps/sketcha/app/demo/page.tsx
+++ apps/sketcha/app/demo/page.tsx
@@ -1,9 +1,11 @@
 import { Canvas } from "../../components/canvas/canvas";
+import { MenuButton } from "../../components/menu/button";
 import { ToolArea } from "../../components/toolbar/toolarea";
 
 export default function DemoPage() {
   return (
     <div className="bg-background h-screen w-screen">
+      <MenuButton />
       <div>
         <ToolArea />
       </div>
--- apps/sketcha/components/menu/button.tsx
+++ apps/sketcha/components/menu/button.tsx
@@ -1,3 +1,25 @@
 "use client";
 
-export function MenuButton() {}
+import { useAtom } from "jotai";
+import { menuAtom } from "../../store/state/state";
+import { Menu } from "lucide-react";
+
+export function MenuButton() {
+  const [menu, setMenu] = useAtom(menuAtom);
+
+  return (
+    <div className="fixed top-4 left-3 md:top-7 md:left-6 z-50">
+      <button
+        type="button"
+        className={`rounded-2xl btn btn-sm md:btn-md border-transparent ${
+          menu
+            ? "bg-[var(--color-selectedtool)] text-white"
+            : "bg-toolarea hover:bg-white/10"
+        }`}
+        onClick={() => setMenu(!menu)}
+      >
+        <Menu className="w-5 h-5 md:w-6 md:h-6" />
+      </button>
+    </div>
+  );
+}
