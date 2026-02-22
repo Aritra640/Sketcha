bf3debab185483ec806db617a94a48ae8d45ed84
Aritra640 <chatterjeearitra15@gmail.com>
2026-02-22 08:13:48 +0530

    minor changes

--- apps/sketcha/components/canvas/canvas.tsx
+++ apps/sketcha/components/canvas/canvas.tsx
@@ -2,15 +2,31 @@
 
 import { useState, useEffect } from "react";
 import { Stage, Layer, Rect } from "react-konva";
+import { toolTypes } from "../../store/types/toolTypes";
+
+interface Shapes {
+  type: toolTypes;
+  prop?: JSON;
+}
 
 export function Canvas() {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [shapes, setShapes] = useState<Shapes[]>([] as Shapes[]);
+  const [curShape, setCurShape] = useState<Shapes>({type: "none"});
+  const [isDrawing, setIsDrawing] = useState<true|false>(false);
 
   useEffect(() => {
-    setDimensions({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
+    const updateWindowSize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    updateWindowSize();
+    window.addEventListener("resize", updateWindowSize);
+
+    return () => window.removeEventListener("resize", updateWindowSize);
   }, []);
 
   return (
