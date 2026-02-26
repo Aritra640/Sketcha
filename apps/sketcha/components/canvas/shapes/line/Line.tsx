daf7e5f0b18c489c193ec47c82e369f79de57335
Aritra640 <chatterjeearitra15@gmail.com>
2026-02-26 11:23:58 +0530

    minor changes

--- apps/sketcha/components/canvas/shapes/line/Line.tsx
+++ apps/sketcha/components/canvas/shapes/line/Line.tsx
@@ -1,6 +1,46 @@
-interface LineProps{}
+import { useAtom } from "jotai";
+import { Shapes } from "../../../../store/types/shapes/shapeProps";
+import { drawnAtom, selectedIdAtom } from "../../../../store/state/state";
+import { Line } from "react-konva";
+import { LineCap } from "konva/lib/Shape";
+import Konva from "konva";
 
-export default function Line() {
+interface LineProps{
+  key: number;
+  shape: Shapes;
+}
 
-  return <div></div>
+export default function LineShape({key, shape}: LineProps) {
+  
+  const [selectedId, setSelectedId] = useAtom(selectedIdAtom);
+  const [drawnShapes, setDrawnShapes] = useAtom(drawnAtom);
+
+  console.log("line id: ", key)
+
+  if(shape.type == "Line") {
+    return  (
+      <Line 
+        id={shape.id}
+        key={shape.id}
+        points={shape.points}
+        stroke={shape.stroke}
+        strokeWidth={shape.strokeWidth}
+        lineCap={shape.LineCap as LineCap} //TODO: type LineCap(konva) might cause error
+        y={shape.y}
+        shadowBlur={shape.shadowBlur}
+        draggable
+        onClick={() => {
+          // e.cancelBubble = true;
+          setSelectedId(shape.id);
+        }}
+        onTap={() => {
+          // e.cancelBubble = true;
+          setSelectedId(shape.id);
+        }}
+
+        onDragEnd={(e) => {
+        }}
+      />
+    );
+  }
 }
