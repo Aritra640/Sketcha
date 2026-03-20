36de0dd9115e51f9beac23a83e5ab798a2ecaa14
Aritra640 <chatterjeearitra15@gmail.com>
2026-03-20 19:43:25 +0530

    authentication

---
+++ apps/server/src/handlers/canvas/crud.ts
@@ -0,0 +1,7 @@
+import {prisma} from "@repo/db_auth_service";
+
+export async function CreateNewCanvas(userId: string, canvasTitle: string) {
+}
+
+export async function UpdateCanvasTitle(canvasTitle: string, newCanvasTitle: string) {
+}
