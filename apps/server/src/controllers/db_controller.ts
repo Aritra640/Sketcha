efb4e0981fb1170e024c5b8d2c0376896258d7d5
Aritra640 <chatterjeearitra15@gmail.com>
2026-03-24 20:31:56 +0530

    serverv0

--- apps/server/src/controllers/db_controller.ts
+++ apps/server/src/controllers/db_controller.ts
@@ -2,28 +2,82 @@ import { prisma } from "@repo/db_auth_service";
 import { ShapeReturn } from "@repo/types/db";
 import { Shapes } from "@repo/types";
 
-export async function AddShape(shape: Shapes, canvasId: string): ShapeReturn {
+export async function AddShape(
+  shape: Shapes,
+  canvasId: string,
+): Promise<ShapeReturn> {
   try {
     const shapeRet = await prisma.shape.create({
       data: {
         type: shape.type,
         canvasId: canvasId,
-        shape: JSON.parse(JSON.stringify(shape)) 
-      }
+        shape: JSON.parse(JSON.stringify(shape)),
+      },
     });
 
-    if(!shapeRet) {
+    if (!shapeRet) {
       throw new Error("AddShape failed!");
     }
 
     return {
       shapeId: shapeRet.id,
       isErr: null,
-      shape: shapeRet.shape
-    }
+      shape: JSON.parse(shapeRet.shape) as Shapes,
+    };
+  } catch (err) {
+    console.log("Error in adding shape: ", err);
+    throw err;
   }
 }
 
-export async function UpdateShape(shapeId: string, shape: Shapes) {}
+export async function UpdateShape(
+  shapeId: string,
+  shape: Shapes,
+): Promise<ShapeReturn> {
+  try {
+    const shapeRet = await prisma.shape.update({
+      where: {
+        id: shapeId,
+      },
+      data: {
+        shape: JSON.stringify(shape),
+      },
+    });
 
-export async function DeleteShape(shapeId: string) {}
+    if (!shapeRet) {
+      throw new Error("shape not found!");
+    }
+
+    return {
+      shapeId: shapeRet.id,
+      isErr: null,
+      shape: JSON.parse(shapeRet.shape) as Shapes,
+    };
+  } catch (err) {
+    console.log("Error in updating shape: ", err);
+    throw err;
+  }
+}
+
+export async function DeleteShape(shapeId: string): Promise<ShapeReturn> {
+  try {
+    const shapeRet = await prisma.shape.delete({
+      where: {
+        id: shapeId,
+      },
+    });
+
+    if (!shapeRet) {
+      throw new Error("shape not found!");
+    }
+
+    return {
+      shapeId: shapeRet.id,
+      isErr: null,
+      shape: JSON.parse(shapeRet.shape) as Shapes,
+    };
+  } catch (err) {
+    console.log("Error in deleting shape: ", err);
+    throw err;
+  }
+}
---
+++ packages/db_auth_service/prisma/migrations/20260324145042_shape_update/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Shape" ALTER COLUMN "shape" SET DATA TYPE TEXT;
