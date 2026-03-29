7054db8ca6aad2d1185de53ecd63d1bac33e7e72
Aritra640 <chatterjeearitra15@gmail.com>
2026-03-29 23:18:59 +0530

    minor changes

--- apps/sketcha/app/api/canvas_state/route.ts
+++ apps/sketcha/app/api/canvas_state/route.ts
@@ -1,3 +1,11 @@
+import { NextResponse } from "next/server";
+
 export async function Get(request: Request) {
   //get all shapes of the current canvas;
+  try{
+
+  }catch(err) {
+    console.error("Error in getting canvas state: ", err);
+    return NextResponse.json({}, {status: 500});
+  }
 }
