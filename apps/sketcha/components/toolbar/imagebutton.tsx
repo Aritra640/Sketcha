fd8d2670e09fb3eb4b35a770ee853296723dcc44
Aritra640 <chatterjeearitra15@gmail.com>
2026-04-25 22:17:39 +0530

    minor changes

--- apps/sketcha/components/toolbar/imagebutton.tsx
+++ apps/sketcha/components/toolbar/imagebutton.tsx
@@ -2,12 +2,14 @@
 
 import { useRef } from "react";
 import { useAtom } from "jotai";
-import { drawnAtom, toolAtom } from "../../store/state/state";
+import { drawnAtom, toolAtom, userDataAtom } from "../../store/state/state";
 import { Image } from "lucide-react";
 import { ToolButton } from "./toolbutton";
 
 export function ImageUploadTool() {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [userData] = useAtom(userDataAtom);
+  const isGuest = userData.isGuest;
 
   const [, setDrawnShapes] = useAtom(drawnAtom);
   const [, setTool] = useAtom(toolAtom);
@@ -48,6 +50,45 @@ export function ImageUploadTool() {
     setTool("cursor");
 
     e.target.value = "";
+  }
+
+  async function handleLocalFile(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    const localUrl = URL.createObjectURL(file);
+
+    setDrawnShapes((prev) => [
+      ...prev,
+      {
+        id: crypto.randomUUID(),
+        type: "Image",
+
+        x: 100,
+        y: 100,
+
+        width: 200,
+        height: 200,
+
+        src: localUrl,
+
+        file,
+        isLocal: true,
+      },
+    ]);
+
+    setTool("cursor");
+
+    e.target.value = "";
+  }
+
+  async function onChangeFunction(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!isGuest) {
+      return handleFile(e);
+    } else {
+      return handleLocalFile(e);
+    }
   }
 
   return (
